8bf29b0b436e8111751fe9674692081294e8ba85
edinhofjr <edinhofassbinder9876@gmail.com>
2025-10-15 16:56:54 -0300

    wip

--- lua/init.lua
+++ lua/init.lua
@@ -1,7 +1,29 @@
+require("utils.functional")
+local debug = require("utils.debug")
 local M = {}
 
+---@alias languages "c" | "cpp" | "javascript" | "lua" | "go" 
+
+---@type table<languages, string>
+local lsp = {
+    ["go"] = "gopls",
+    ["lua"] = "lua-language-server"
+}
+
+
+---@class opts
+---@field ensure_installed? languages[]
+
+---@param opts opts
 M.setup = function(opts)
-    print(vim.inspect(opts))
+    local success, mason = pcall(require, "mason.api.command")
+
+    if not success then
+        vim.notify("ERROR: mason is not installed")
+        return
+    end
+
+    vim.cmd("nvim")
 end
 
 return M
